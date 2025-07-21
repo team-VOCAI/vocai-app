22ef51ff98b96886560b3f1fe61814cd99024e68
ssini-oh <ssini8280@gmail.com>
2025-07-21 15:18:47 +0900

    feat: eslint 무시 옵션 추가

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  
+  // eslint 무시하는 옵션
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
